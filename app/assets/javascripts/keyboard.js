99a2d157b4847a5840c302a21d75900f16fa4123
JaimeContardo <jcontardo@miuandes.cl>
2019-05-08 15:04:45 -0400

    New Commit

--- app/assets/javascripts/keyboard.js
+++ app/assets/javascripts/keyboard.js
@@ -1,4 +1,41 @@
 // Note: $(() => {}); is equivalent to $(document).ready(function(){})
-$(() => {
-	console.log('Document ready! [app/assets/javascripts/keyboard.js]');
+function write_on_console(){
+	$(".key").click(function(){
+		console.log($(this).text());
+	});
+};
+
+function write(){
+	$(".key").click(function(){
+		var $text = $("#text");
+
+		var $this = $(this),
+			character = $this.html();
+
+
+		if (character=="Shift") {
+			shift = true;
+			character = "";
+		}
+
+		if (character==" ") character = " ";
+		if (character=="Tab") character = "\t";
+		if (character=="Enter") character = "\n";
+
+		if (character=="Bksp") {
+			var html = $text.html();
+			
+			$text.html(html.substr(0, html.length - 1));
+			return false;
+		}
+
+		$text.html($text.html() + character);
+		shift = false;
+	});
+
+};
+
+$(document).ready(function(){
+	write_on_console();
+	write();
 });
